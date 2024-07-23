189c6043b58026964f679e3fe97ea8986c63b538
(iam)Stephen Sams <iam_samss@devlearningnext.com>
2024-07-23 16:49:02 -0500

    current Api exercise progress

---
+++ DataForCountries/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [currFilter, setCurrFilter] = useState('')
+
+  useEffect(() => {
+        axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+        .then(response => {
+          setCountries(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching countries:', error);
+        })
+    });
+
+    const handleFilterChange = (event) => {
+      setCurrFilter(event.target.value)
+    }
+
+    const filteredCountries = countries.filter(country => {
+      const regex = new RegExp(currFilter, 'i')
+      return regex.test(country.name.common)
+    })
+
+  return (
+    <div>
+      <input 
+         type="text" 
+         value={currFilter} 
+         onChange={handleFilterChange} />
+      {filteredCountries.length > 10 ? (
+        <p>Too many matches, specify another filter</p>
+      ) : (
+        <ul>
+          {filteredCountries
+            .map((country) => (
+            <li key={country.ccn3}>{country.name.common}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+
+}
+
+export default App;
