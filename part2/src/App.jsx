8d4d47d049a02e2c2c9a8ea711bbbeea11fd2a40
(iam)Stephen Sams <iam_samss@devlearningnext.com>
2024-07-23 09:47:48 -0500

    Added the ability to alter data from the 'server'

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -9,15 +10,13 @@ const App = () => {
   )
   const [showAll, setShowAll] = useState(false)
 
-  const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }
+ useEffect(() => {
+    noteService
+       .getAll()
+       .then(initialNotes => {
+        setNotes(initialNotes)
+       })
+ }, [])
   
   useEffect(hook, [])
 
@@ -27,29 +26,33 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      // id: String(notes.length + 1)
+      
     }
 
-    // setNotes(notes.concat(noteObject))
-    // setNewNote('')
-    axios
-    .post('http://localhost:3001/notes', noteObject)
-    .then(response => {
-      setNotes(notes.concat(response.data))
+    noteService
+    .create(noteObject)
+    .then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
       setNewNote('')
-      console.log(response)
     })
   }
 
   const toggleImportanceOf = (id) => {
     console.log(`importance of ${id} needs to be toggled`)
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = {...note, important: !note.important}
     
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(n=> n.id !== id ? n : response.data))
-    })
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
   }
 
   const handleNoteChange = (event) => {
