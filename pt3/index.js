71809ab3bf55ced893515024287ce7e948fcfe48
(iam)Stephen Sams <iam_samss@devlearningnext.com>
2024-07-26 10:26:18 -0500

    made production build

--- pt3/index.js
+++ pt3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('dist'))
+
 let notes = [
     {
         id: "1",
@@ -99,7 +101,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
