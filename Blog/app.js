5fbe5b218a3874281c2f7d5f1aac5baf1042fcbd
(iam)Stephen Sams <iam_samss@devlearningnext.com>
2024-08-08 13:32:36 -0500

    Started adding Login and User functionality to 'Blog'

--- Blog/app.js
+++ Blog/app.js
@@ -1,8 +1,11 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
@@ -20,6 +23,8 @@ mongoose.connect(config.MONGO_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 
