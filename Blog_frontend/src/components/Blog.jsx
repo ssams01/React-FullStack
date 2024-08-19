ff8bd1b7949b1bb41b5ce6d6a824da6754b62cad
(iam)Stephen Sams <iam_samss@devlearningnext.com>
2024-08-19 08:24:52 -0500

    Users can now toggle visibilty of all of a blogs fields

--- Blog_frontend/src/components/Blog.jsx
+++ Blog_frontend/src/components/Blog.jsx
@@ -1,9 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-    {blog.url}
-    {blog.likes}
-  </div>  
-)
+import React, { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(true);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+      </div>
+      {showDetails && (
+        <div>
+          <button onClick={toggleDetails}>hide</button>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes}</p>
+        </div>
+      )}
+      {!showDetails && (
+        <div>
+          <button onClick={toggleDetails}>view</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
