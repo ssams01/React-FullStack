42554d9af4688354d572c4cb35a5deab1169b5b8
(iam)Stephen Sams <iam_samss@devlearningnext.com>
2024-08-19 08:47:47 -0500

    added the ability to like a blog (needs to be tested still)

--- Blog_frontend/src/components/Blog.jsx
+++ Blog_frontend/src/components/Blog.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+//import blogService from './services/blogs
 
 const Blog = ({ blog }) => {
   const [showDetails, setShowDetails] = useState(true);
+  const [likes, setLikes] = useState(blog.likes)
 
   const blogStyle = {
     paddingTop: 10,
@@ -15,6 +17,18 @@ const Blog = ({ blog }) => {
     setShowDetails(!showDetails);
   };
 
+  const handleLikes = async () => {
+    const updatedBlog = {...blog, likes: likes + 1}
+
+    try {
+      const returnedBlog = await blogService.update(updatedBlog)
+      setLikes(returnedBlog.likes)
+    }
+    catch (error) {
+      console.error('Error updating blog:', error)
+    }
+  }
+
   return (
     <div style={blogStyle}>
       <div>
@@ -24,7 +38,7 @@ const Blog = ({ blog }) => {
         <div>
           <button onClick={toggleDetails}>hide</button>
           <p>{blog.url}</p>
-          <p>likes {blog.likes}</p>
+          <p>likes {blog.likes} <button onClick={handleLikes}>like</button></p>
         </div>
       )}
       {!showDetails && (
