043d9b4762688f0743e7754891aefd2cd68a3780
(iam)Stephen Sams <iam_samss@devlearningnext.com>
2024-08-20 11:56:09 -0500

    started adding PropTypes to 'Note' frontend

--- part2/src/components/Togglable.jsx
+++ part2/src/components/Togglable.jsx
@@ -1,6 +1,7 @@
 import { useState, forwardRef, useImperativeHandle } from 'react'
+import PropTypes from 'prop-types'
 
-const Togglable = forwardRef((props,refs) => {
+const Togglable = React.forwardRef((props,refs) => {
     const [visible, setVisible] = useState(false)
 
     const hideWhenVisible = { display: visible ? 'none' : ''}
@@ -28,5 +29,9 @@ const Togglable = forwardRef((props,refs) => {
         </div>
       )
 })
+
+Toggable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+}
 
 export default Togglable
